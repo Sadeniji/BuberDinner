e463de0199bc99e23b287d618868a485eed199fb
Saheed Adeniji <adeniji4success@gmail.com>
2023-06-11 01:13:51 +0100

    Implement generating jwt tokens

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,14 +1,30 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
-    public AuthenticationResult Login(string email, string password)
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
     {
-        return new AuthenticationResult(Guid.NewGuid(), "", "", email, "token");
+        _jwtTokenGenerator = jwtTokenGenerator;
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+        // Check if user already exists
+
+        // Create user (generate unique Id)
+
+        // Create Jwt token
+        var userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, token);
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    {
+        return new AuthenticationResult(Guid.NewGuid(), "", "", email, "token");
     }
 }   
