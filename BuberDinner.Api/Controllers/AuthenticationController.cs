f0c5884779028825af1d99cf70460e667ca4b1f4
Saheed Adeniji <adeniji4success@gmail.com>
2023-06-18 12:57:07 +0100

    Implement Add and Get User in UserRepository file

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -19,13 +19,19 @@ public class AuthenticationController : ControllerBase
     public IActionResult Register(RegisterRequest request)
     {
         var authResult = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.Password);
-        return Ok(authResult);
+
+        var response = new AuthenticationResponse(authResult.User.Id, authResult.User.FirstName,
+                                                  authResult.User.LastName, authResult.User.Email, authResult.Token);
+        return Ok(response);
     }
 
     [HttpPost("login")]
     public IActionResult Login(LoginRequest request)
     {
         var authResult = _authenticationService.Login(request.Email, request.Password);
-        return Ok(authResult);
+
+        var response = new AuthenticationResponse(authResult.User.Id, authResult.User.FirstName,
+                                                  authResult.User.LastName, authResult.User.Email, authResult.Token);
+        return Ok(response);
     }
 }
---
+++ Docs/DomainModels.md
@@ -0,0 +1,15 @@
+# Domain Models
+
+## User
+
+```json
+{
+    "id": "0000000-00000-00000-0000-00000000",
+    "firstName": "Amichai",
+    "lastName": "Mantinband",
+    "email": "amichai@mantinband.com",
+    "password": "Amiko1231!"
+}
+```
+
+> Note: Passwords shouldn't be laying around like this
