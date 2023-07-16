af230150975438dbafc077951140685f3d99b985
Saheed Adeniji <adeniji4success@gmail.com>
2023-07-16 08:46:59 +0100

    Add Global Error handling

---
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class ErrorsController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            return Problem();
+        }
+    }
+}
